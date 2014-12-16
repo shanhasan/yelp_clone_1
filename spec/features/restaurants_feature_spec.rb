0d14503943525f35edee3fb11345120c76965cda
Shan Hasan <shan119@hotmail.co.uk>
2014-12-16 12:48:25 +0000

    added first test and routing

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails-helper'
+
+feature 'restaurants' do
+  context 'no resturants have been added' do
+    scenario 'should displat a prompt to add a resturant' do
+      visit '/resturants'
+      expect(page).to have_content 'No resturants'
+      expect(page).to have_content 'Add a resturant'
+    end
+  end
+end
